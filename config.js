1bee839cdc9c01380754c16f6dcb8fce3a86a959
yeuminhanh <109031527+yeuminhanh@users.noreply.github.com>
2022-07-10 21:40:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Connecting from Love server",
+    introTitle: 'Minh Anh yêu ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
